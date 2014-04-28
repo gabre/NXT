60a28fe04077c44e1bc6b5e213bf7ca1993a9aaf
gabre <gabre@caesar.elte.hu>
2014-04-28 17:26:34 +0200

    Add start synching

--- gui/MainWindow.java
+++ gui/MainWindow.java
@@ -4,11 +4,13 @@ import unused.SimpleConsoleLogger;
 import de.se.NxtBrickBluetoothThread;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -32,17 +34,29 @@ public class MainWindow extends Application {
         
         myNxt = new NxtBrickBluetoothThread(new SimpleConsoleLogger(), myBrick);
         myNxt.addObserver(mapDrawer);
-        Thread nxtBluetoothThread = new Thread(myNxt);
+        final Thread nxtBluetoothThread = new Thread(myNxt);
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 			@Override
 			public void handle(WindowEvent event) {
 				myNxt.terminate();
 			}
         });
+        
         nxtBluetoothThread.start();
         
+        Button start = new Button("START");
+        
+        
+        start.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				String msg = "START";
+				myNxt.send(0, msg);
+			}
+        });
+        
         mapDrawer.drawShapes();
-        root.getChildren().add(canvas);
+        root.getChildren().addAll(canvas, start);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
